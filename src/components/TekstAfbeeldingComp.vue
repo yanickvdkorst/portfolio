a214d4b7a1ea63bfc2d2017f22da14bea08fe9df
Yanick van der Korst <>
2025-02-16 21:09:25 +0100

    added tekst met afbeelding

---
+++ src/components/TekstAfbeeldingComp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="tekst-met-afbeelding" :class="`image-${imagePosition}`">
+        <div class="container">
+            <div class="row">
+                <div v-if="imagePosition === 'left'" class="col-lg-4">
+                    <div class="image-container">
+                        <img src="@/assets/images/profile.jpg" alt="profile">
+                    </div>
+                </div>
+
+
+                <div :class="colClass">
+                    <div class="content">
+                        <h2 class="title">Wie ben ik</h2>
+                        <p class="tekst">Ik ben Yanick van der Korst, 21 jaar oud en wonende in Sint-Oedenrode. Ik studeer aan de Fontys Hogeschool ICT Media & Design en daarnaast ben ik part time front-end developer bij Digitale Bazen. Voor het afronden van mijn studie ben ik opzoek naar een stage. Benieuwd naar mijn wensen? Klik dan op onderstaande link. </p>
+                        <a class="button secondary" href="#">Meer over mij</a>
+                    </div>
+                </div>
+
+                <div v-if="imagePosition === 'right'" class="offset-lg-1 col-lg-4">
+                    <div class="image-container">
+                        <img src="@/assets/images/profile.jpg" alt="profile">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+    const props = defineProps<{
+    imagePosition?: "left" | "right";
+    }>();
+
+    // Bepaal de CSS-klasse afhankelijk van de imagePosition
+    const colClass = computed(() => {
+    return props.imagePosition === "left" ? "offset-lg-1 col-lg-7" : "col-lg-7";
+    });
+</script>
--- src/modules/pages/HomePage.vue
+++ src/modules/pages/HomePage.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 // import Hello from "../components/Hello.vue"
 import Banner from "@/components/BannerComp.vue";
+import TekstMetAfbeelding from "@/components/TekstAfbeeldingComp.vue"
 
 
 </script>
 
 <template>
   <Banner />
+  <TekstMetAfbeelding imagePosition="right" />
+
+
 </template>
